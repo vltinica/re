823dfab5b98f90ff67753e16fbb026b717505f1f
Vladimir Tinica <vltinica@gmail.com>
2024-08-30 08:04:11 -0400

    *

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectsSidebar = () => {
   return (
-    <aside>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2>You Projects</h2>
       <div>
         <button> + Add Project</button>
