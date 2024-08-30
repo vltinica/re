9f64acf68b35db07c9d9119d04bae5a5394744b8
Vladimir Tinica <vltinica@gmail.com>
2024-08-30 07:02:29 -0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSidebar/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectsSidebar = () => {
+  return (
+    <aside>
+      <h2>You Projects</h2>
+      <div>
+        <button> + Add Project</button>
+      </div>
+      <ul>...</ul>
+    </aside>
+  );
+};
+
+export default ProjectsSidebar;
