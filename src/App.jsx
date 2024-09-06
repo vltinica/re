7cb0f7a40855b2d4b35fb52f0e4ece31f282ad29
Vladimir Tinica <vltinica@gmail.com>
2024-09-05 22:02:52 -0400

    modified our components

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,23 @@ function handleStartAddProject() {
   })
 }
 
+function handleAddProject(projectData) {
+  setProjectsState(prevState => {
+    const newProject = {
+      ...projectData,
+      id: Math.random()
+    }
+    return({
+      ...prevState,
+      projects: [...prevState.projects, newProject]
+    })
+  })
+}
+
 let content;
 
 if (projectsState.selectedProjectId === null) {
-  content = <NewProject />
+  content = <NewProject onAdd={handleAddProject} />
 }else if(projectsState.selectedProjectId === undefined) {
   content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { forwardRef } from "react";
 
-const Input = ({ label, textarea, ...props }) => {
+
+
+const Input = forwardRef (({ label, textarea, ...props }, ref) => {
 
 const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
 
@@ -10,10 +13,10 @@ const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 
         <label className="text-sm font-bold uppercase text-stone-500">
           {label}
         </label>
-        {textarea ? <textarea className={classes} {...props} /> : <input className={classes} {...props} />}
+        {textarea ? <textarea ref={ref} className={classes} {...props} /> : <input ref={ref} className={classes} {...props} />}
       </p>
     </div>
   );
-};
+});
 
 export default Input;
